1f66028c76ea4e4f477c6396c524662ba3db223e
Ikey Doherty <ikey@serpentos.com>
2022-06-30 01:22:33 +0100

    Add initial vibe.d skeleton
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/app.d
@@ -0,0 +1,21 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2020-2022 Serpent OS Developers
+ *
+ * SPDX-License-Identifier: Zlib
+ */
+
+/**
+ * main
+ *
+ * Main entry point into Avalanche
+ *
+ * Authors: Copyright © 2020-2022 Serpent OS Developers
+ * License: Zlib
+ */
+
+module main;
+
+void main()
+{
+
+}
