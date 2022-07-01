37a3065251cb688a9790ea1600b8b8241f1193e0
Ikey Doherty <ikey@serpentos.com>
2022-07-01 16:10:04 +0100

    test_node: Update for JWT
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- test_node.sh
+++ test_node.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 curl -X PUT localhost:8081/api/v1/node/build_bundle -H "Content-Type: application/json" \
+    -H "Authorization: Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhdmFsYW5jaGUtbm9kZSIsImlzcyI6IlNlcnBlbnRPUy9Ob2RlL2xvY2FsaG9zdCIsImlhdCI6MTUxNjIzOTAyMiwiZXhwIjoxNTE2MjM5MDIyfQ.t7r3u3W0vwUVBiu50tfHnul4xmCyI3iN6b2AXQwx2M7oQGwapcAsmvvb_YZFRtHQsWiZRS5FFJAgHJhgEbeBvw" \
     -d '{"bundle": {"originType": "git", "remoteIdentifier": 123, "originURI": "git@gitlab.com:serpent-os/recipes/recipes.git", "recipePath":"nano/stone.yml", "architecture":"x86_64", "originRef":"bc3c16d61607b31dac3b0c21b93beefd213e9cf1EmA"}}'
