6e9a028829a4382b6a1ae5d948593dd9558816ba
Ikey Doherty <ikey@serpentos.com>
2022-07-02 00:43:40 +0100

    auth: Begin work on an RNG helper
    We'll use the RNG to correctly seed and generate 512-bit secrets.
    
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/avalanche/auth/rng.d
@@ -0,0 +1,43 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2020-2022 Serpent OS Developers
+ *
+ * SPDX-License-Identifier: Zlib
+ */
+
+/**
+ * avalanche.auth.rng
+ *
+ * Random Number Generation helpers
+ *
+ * Authors: Copyright © 2020-2022 Serpent OS Developers
+ * License: Zlib
+ */
+
+module avalanche.auth.rng;
+
+import deimos.openssl.rand;
+import std.concurrency : initOnce;
+
+private __gshared RandomNumberGenerator _rng;
+
+/**
+ * Return the shared RNG
+ */
+public static RandomNumberGenerator RNG() @trusted
+{
+    return initOnce!_rng(new RandomNumberGenerator());
+}
+
+shared static ~this()
+{
+    _rng.destroy();
+    _rng = null;
+}
+
+/**
+ * Lose abstraction around openssl rand behaviour
+ */
+public final class RandomNumberGenerator
+{
+
+}
