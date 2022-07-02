12e60ff56d4a6f5957ff75ea876a42be1da48491
Ikey Doherty <ikey@serpentos.com>
2022-07-02 13:20:49 +0100

    auth/rng: Implement our RNG wrapper
    Currently this will always regenerate the seed on startup
    which isn't what we actually want. We'll want to store our
    RNG seed as state for generating new tokens to "talk" to
    other services.
    
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/avalanche/auth/rng.d
+++ source/avalanche/auth/rng.d
@@ -17,6 +17,11 @@ module avalanche.auth.rng;
 
 import deimos.openssl.rand;
 import std.concurrency : initOnce;
+import std.exception : enforce, assumeUnique;
+import std.random : rndGen;
+import std.range : take;
+import std.array : array;
+import std.digest : toHexString, LetterCase;
 
 private __gshared RandomNumberGenerator _rng;
 
@@ -35,9 +40,65 @@ shared static ~this()
 }
 
 /**
+ * 512-bit keys
+ */
+private enum BitSize = 512 / 8;
+/**
  * Loose abstraction around openssl rand behaviour
  */
 public final class RandomNumberGenerator
 {
 
+public:
+
+    /**
+     * Return a newly generated key with the given size
+     */
+    ubyte[BitSize] generate() @safe
+    {
+        ubyte[BitSize] buffer;
+        int rc = () @trusted {
+            return RAND_bytes(&buffer[0], cast(int) buffer.sizeof);
+        }();
+        enforce(rc == 1, "RNG.generate(): Failed to generate random bytes");
+        return buffer;
+    }
+
+    /**
+     * Generate a new key encoded as hex
+     */
+    string generateHex() @safereturn
+    {
+        ubyte[BitSize] buffer = generate();
+        char[BitSize * 2] hexBuffer = toHexString!(LetterCase.lower)(buffer);
+        return hexBuffer.dup;
+    }
+
+package:
+
+    this() @trusted
+    {
+        /* Ensure RAND is initialised */
+        RAND_poll();
+
+        /* Initialise RNG */
+        auto rx = RAND_load_file("/dev/random", BitSize);
+        enforce(rx == BitSize, "RNG(): Failed to load random data");
+
+        regenerateSeed();
+    }
+
+private:
+
+    /**
+     * Regenerate the seed using dlang functions
+     */
+    void regenerateSeed()
+    {
+        auto dRng = rndGen();
+        immutable(uint[]) sequence = assumeUnique(dRng.take(BitSize).array);
+
+        /* Seed OpenSSL RAND */
+        RAND_seed(cast(void*) sequence.ptr, BitSize);
+    }
 }
