be240644daea72ffcba86477a30c7acaa1c66553
Ikey Doherty <ikey@serpentos.com>
2022-09-15 11:16:32 +0100

    Update test script for upcoming functionality
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- test_builder.sh
+++ test_builder.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-curl -X PUT localhost:8081/api/v1/build/build_bundle -H "Content-Type: application/json" \
-    -H "Authorization: Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhdmFsYW5jaGUtbm9kZSIsImlzcyI6IlNlcnBlbnRPUy9Ob2RlL2xvY2FsaG9zdCIsImlhdCI6MTUxNjIzOTAyMiwiZXhwIjoxNTE2MjM5MDIyfQ.t7r3u3W0vwUVBiu50tfHnul4xmCyI3iN6b2AXQwx2M7oQGwapcAsmvvb_YZFRtHQsWiZRS5FFJAgHJhgEbeBvw" \
-    -d '{"bundle": {"originType": "git", "remoteIdentifier": 123, "originURI": "git@gitlab.com:serpent-os/recipes/recipes.git", "recipePath":"nano/stone.yml", "architecture":"x86_64", "originRef":"bc3c16d61607b31dac3b0c21b93beefd213e9cf1EmA"}}'
+curl -X POST localhost:8082/api/v1/build_package -H "Content-Type: application/json" \
+    -d '{"request": {"buildID": 0, "uri": "https://github.com/snekpit/main", "commitRef" : "8ba4eaec63e8bbb7e5c9c22712f9a696bb5d85e4", "relativePath" : "base/zlib/stone.yml", "collections": [{"indexURI": "https://dev.serpentos.com/protosnek/x86_64/stone.index", "name": "protosnek", "priority" : 0}], "buildArchitecture": "x86_64"}}'
