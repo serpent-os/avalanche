63ef4c805f89c7101c8de365e5f3c313bb7514fa
Ikey Doherty <ikey@serpentos.com>
2022-11-14 15:06:40 +0000

    test_builder: Set proper commitRef for zlib test
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- test_builder.sh
+++ test_builder.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 curl -X POST localhost:8082/api/v1/build_package -H "Content-Type: application/json" \
-    -d '{"request": {"buildID": 0, "uri": "https://github.com/snekpit/main", "commitRef" : "8ba4eaec63e8bbb7e5c9c22712f9a696bb5d85e4", "relativePath" : "base/zlib/stone.yml", "collections": [{"indexURI": "https://dev.serpentos.com/protosnek/x86_64/stone.index", "name": "protosnek", "priority" : 0}], "buildArchitecture": "x86_64"}}'
+    -d '{"request": {"buildID": 0, "uri": "https://github.com/snekpit/main", "commitRef" : "262dc2c4c6edd4620e6eb03ad7d3aedba6e80df7", "relativePath" : "base/zlib/stone.yml", "collections": [{"indexURI": "https://dev.serpentos.com/protosnek/x86_64/stone.index", "name": "protosnek", "priority" : 0}], "buildArchitecture": "x86_64"}}'
