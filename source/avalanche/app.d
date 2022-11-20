8febb4d4a98416b0d0218c19d735dc74b96d0a7b
Ikey Doherty <ikey@serpentos.com>
2022-11-20 23:14:59 +0000

    app: Port to new retrieveToken API
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/avalanche/app.d
+++ source/avalanche/app.d
@@ -111,7 +111,7 @@ public final class AvalancheApp
      */
     void errorHandler(HTTPServerRequest req, HTTPServerResponse res, HTTPServerErrorInfo error) @safe
     {
-        immutable bool needLogin = error.code == HTTPStatus.forbidden && req.requestToken.isNull;
+        immutable bool needLogin = error.code == HTTPStatus.forbidden && retrieveToken(req, res).isNull;
 
         if (needLogin)
         {
